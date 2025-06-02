8d1ec67722e1613677062e45c083a785c1add049
Alexander Stamnes <alesta81@yahoo.com>
2025-06-02 15:36:29 +0200

    Add dotenv-rails

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
 import { Application } from "@hotwired/stimulus"
+import "bootstrap"
+import "@popperjs/core"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+
 
 export { application }
