ef37b4363fd4b1e01e592bc675e0041db1e65345
Alexander Stamnes <alesta81@yahoo.com>
2025-06-04 16:44:04 +0200

    showpage done. Missing: Rating and location if we want to have it

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,4 +1,7 @@
 class FishController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_fish, only: [:show]
+
+  def show
+    @fish = Fish.find(params[:id])
+  end
 end
