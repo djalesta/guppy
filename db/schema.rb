b156a42c2f1ba6f073a2f229b8c9e4028673bfba
Alexander Stamnes <alesta81@yahoo.com>
2025-06-05 10:55:58 +0200

    updated database config

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_06_03_085124) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,7 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_085124) do
     t.float "length"
     t.float "latitude"
     t.float "longitude"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.boolean "availability"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -59,8 +62,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_085124) do
     t.date "end_date"
     t.string "status"
     t.datetime "created_date"
-    t.integer "fish_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "fish_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["fish_id"], name: "index_matches_on_fish_id"
@@ -71,8 +74,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_085124) do
     t.text "content"
     t.boolean "read_status"
     t.datetime "date_created"
-    t.integer "match_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "match_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["match_id"], name: "index_messages_on_match_id"
@@ -82,8 +85,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_085124) do
   create_table "reviews", force: :cascade do |t|
     t.integer "score"
     t.text "comment"
-    t.integer "user_id", null: false
-    t.integer "match_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "match_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["match_id"], name: "index_reviews_on_match_id"
