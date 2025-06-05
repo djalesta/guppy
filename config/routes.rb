2421ac9e9fb5e6ca453f5daa93ac21135330f5ee
Zeynep ÇaCaliskan <zeynepcaliskan@hacettepe.edu.tr>
2025-06-05 17:57:44 +0300

    settings, my profile, some front end arrangements

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-   root to: "pages#landing"
+  root to: "pages#landing"
+  get '/profile', to: 'profiles#show', as: :profile
+  get "frontend-conventions", to: "frontend#displayconventions"
+  get "/inbox", to: "messages#inbox", as: :inbox
 
-   get "frontend-conventions", to: "frontend#displayconventions"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :user_fish
   resources :fish
   resources :users
+  resources :matches
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
