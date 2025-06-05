8f8611ff1f0e31faf031b620f9cf7ec31db77e6b
Alexander Stamnes <alesta81@yahoo.com>
2025-06-05 15:55:58 +0200

    map works some. Need to add picture to the pop up for each fish

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -3,10 +3,16 @@ class FishController < ApplicationController
 
   def show
     @fish = Fish.find(params[:id])
+     @markers = @fish.geocoded.map do |flat|
+    {
+      lat: flat.latitude,
+      lng: flat.longitude,
+      info_window_html: render_to_string(partial: "info_window", locals: {fish: fish})
+    }
+    end
   end
 
   def index
     @fish = Fish.all
   end
-
 end
---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus";
+import mapboxgl from "mapbox-gl";
+
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  };
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue;
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    });
+
+    this.#addFishToMap();
+    this.#fitMapToMarkers()
+  }
+
+  #addFishToMap() {
+    this.markersValue.forEach((fish) => {
+      console.log(fish.info_window_html); // ✅ Confirm this is not undefined
+      const popup = new mapboxgl.Popup().setHTML(fish.info_window_html)
+      new mapboxgl.Marker()
+        .setLngLat([fish.lng, fish.lat])
+        .setPopup(popup)
+        .addTo(this.map);
+    });
+  }
+
+  #fitMapToMarkers() {
+  const bounds = new mapboxgl.LngLatBounds()
+  this.markersValue.forEach(fish => bounds.extend([ fish.lng, fish.lat ]))
+  this.map.fitBounds(bounds, { padding: 40, maxZoom: 15, duration: 0 })
+  }
+}
