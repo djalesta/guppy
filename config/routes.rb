aeb0a46b2c4f110bcba7cda3af04b52663d60522
David Lou <davidlou2006@gmail.com>
2025-06-05 22:52:15 +1200

    backend for match done

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,18 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :user_fish
-  resources :fish
+
+  resources :fish do
+    resources :matches
+
+    member do
+      get :match
+    end
+
+  end
+
   resources :users
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
