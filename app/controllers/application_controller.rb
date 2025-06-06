c5e3f69bcfa9cdfb4fbfe246be981f96991f7d83
Alexander Stamnes <alesta81@yahoo.com>
2025-06-06 13:02:21 +0200

    fixed links in fish show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,14 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  def after_sign_in_path_for(user)
+    fish_index_path
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :photo])
-  end
-
-  def after_sign_in_path_for(user)
-    fish_index_path
   end
 end
