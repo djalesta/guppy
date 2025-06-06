a552c0639d2debb31128a234eb349c9bc8151ae7
Alexander Stamnes <alesta81@yahoo.com>
2025-06-06 11:00:45 +0200

    fixed error in fish controller

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -3,13 +3,6 @@ class FishController < ApplicationController
 
   def show
     @fish = Fish.find(params[:id])
-     @markers = @fish.geocoded.map do |flat|
-    {
-      lat: flat.latitude,
-      lng: flat.longitude,
-      info_window_html: render_to_string(partial: "info_window", locals: {fish: fish})
-    }
-    end
   end
 
   def index
